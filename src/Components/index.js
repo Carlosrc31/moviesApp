55f614158346085358ca9f68ace7ac7ebb7d836d
Carlosrc31 <charlie31rc@gmail.com>
2021-11-23 15:56:04 -0600

    Solución de error

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,4 +1,3 @@
 export { default as Homepage } from "./Homepage/Homepage.component";
 export { default as Movie } from "./Movie/Movie.component";
 export { default as MovieInfo } from "./MovieInfo/MovieInfo.component"; 
-export { default as ScrollButton} from "./ScrollToTop/ScrollToTop.componet"
